da91a6dd44e52a0638ac65df31e38e281ab3b40a
CrazyUs3r <91020381+CrazyUs3r@users.noreply.github.com>
2026-02-06 18:27:02 +0100

    Update language.go

--- language.go
+++ language.go
@@ -71,6 +71,7 @@ func loadLanguage(lang string) error {
 }
 
 func toSnakeCase(s string) string {
+	var result []rune
 	for i, r := range s {
 		if i > 0 && unicode.IsUpper(r) {
 			result = append(result, '_')
