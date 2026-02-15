06c0a1c83b4a4556b8b5db5eba38a44166a5651f
CrazyUs3r <91020381+CrazyUs3r@users.noreply.github.com>
2026-02-15 15:26:57 +0100

    Update globals.go

--- globals.go
+++ globals.go
@@ -76,9 +76,6 @@ var (
 
 	lastIPv64Update     time.Time
 	ipv64Mutex          sync.Mutex
-	ipv64RateLimitUntil atomic.Value
-	ipv64RateLimitCount atomic.Int32
-	ipv64RateLimitMutex sync.Mutex
 
 	wsHub = &WSHub{
 		clients:    make(map[*WSClient]bool),
