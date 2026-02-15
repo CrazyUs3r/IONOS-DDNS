a7b60f2114716f2191c3fa315ba9f4991d550227
CrazyUs3r <91020381+CrazyUs3r@users.noreply.github.com>
2026-02-15 16:06:21 +0100

    Update http_client.go

--- http_client.go
+++ http_client.go
@@ -138,7 +138,7 @@ func getHTTPClient() *http.Client {
 			KeepAlive: DNSKeepalive,
 			Resolver: &net.Resolver{
 				PreferGo: true,
-				Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+				Dial: func(ctx context.Context, _, address string) (net.Conn, error) {
 					var lastErr error
 					startIndex := int(atomic.LoadInt32(&lastSuccessfulDNS))
 
