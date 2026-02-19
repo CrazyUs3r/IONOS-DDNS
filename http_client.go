e07d9b863768f1ebef48d78b822528ccceef1c91
CrazyUs3r <91020381+CrazyUs3r@users.noreply.github.com>
2026-02-19 12:53:09 +0100

    Fix formatting of ConnectDone function parameters

--- http_client.go
+++ http_client.go
@@ -45,7 +45,7 @@ func (t *httpTimings) trace() *httptrace.ClientTrace {
 				t.connectAddr = addr
 			}
 		},
-		ConnectDone: func(_ , addr string, err error) {
+		ConnectDone: func(_, addr string, err error) {
 			if t.connectDone.IsZero() {
 				t.connectDone = time.Now()
 				t.connectErr = err
