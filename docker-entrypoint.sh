c1eb23ea6437678f6015807897bd6de3a82a845a
CrazyUs3r <91020381+CrazyUs3r@users.noreply.github.com>
2026-06-14 20:41:42 +0200

    Simplify entrypoint by removing su-exec check
    Removed the condition to use su-exec for executing the dyndns application.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,9 +17,6 @@ if [ "$(id -u)" = "0" ]; then
         echo "Adjusting ownership of $CONFIG_DIR"
         chown -R 1000:1000 "$CONFIG_DIR"
     fi
-    if [ -f /sbin/su-exec ]; then
-        exec /sbin/su-exec dyndns /app/dyndns "$@"
-    else
         exec su -s /bin/sh -c "/app/dyndns $@" dyndns
     fi
 fi
